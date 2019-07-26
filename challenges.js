7e557c2c112206043a7553bb15d9db8fca36f7ad
Vittoria Erdem <vittoria@Vittorias-MacBook-Pro.local>
2019-07-26 10:51:12 -0500

    Solve 06

--- challenges.js
+++ challenges.js
@@ -217,6 +217,9 @@ reverseUpcaseString("SEI Rocks!"); //=> "!SKCOR IES"
 -----------------------------------------------------------------*/
 // Your solution for 07-reverseUpcaseString here:
 
+function reverseUpcaseString(str){
+  return str.split('').reverse().join('').toUpperCase()
+}
 
 
 /*-----------------------------------------------------------------
